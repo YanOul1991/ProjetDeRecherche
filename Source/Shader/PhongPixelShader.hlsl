25ae0bb6f8b73005ae04dc1681137ad11f279f64
Yanis <e2073117@cmaisonneuve.qc.ca>
2026-01-21 17:58:46 -0500

    Updates to lit pixel shader.

--- Source/Shader/PhongPixelShader.hlsl
+++ Source/Shader/PhongPixelShader.hlsl
@@ -1,18 +1,19 @@
 // PHONG PIXEL SHADER
 
-static const float3 lightPosition = { 0.0f, 1.0f, 1.0f }; // Position of the light souce in WorldSpace
-static const float3 materialColor = { 0.75f, 0.75f, 0.75f }; // Color of the fragment (For now non textures)
+static const float3 lightPosition = { 0.0f, 2.0f, 10.0f }; // Position of the light souce in WorldSpace
+static const float3 materialColor = { 0.0f, 1.0f, 1.0f }; // Color of the fragment (For now non textures)
 
 static const float3 ambient = { 0.15f, 0.15f, 0.15f };
 
-static const float3 diffuseColor = { 1.0f, 0.75f, 0.52f }; // Light Color
-static const float diffuseIntensity = 1.0f;              // Light intensity
+// static const float3 diffuseColor = { 1.0f, 0.75f, 0.52f }; // Light Color
+static const float3 diffuseColor      = { 1.0f, 0.6f, 1.0f }; // Light Color
+static const float  diffuseIntensity  = 5.0f;              // Light intensity ,k
 
-static const float attenuation_constant   = 0.5f;
-static const float attenuation_linear     = 0.0f;
-static const float attenuation_quadtraic  = 0.1f;
+static const float attenuation_constant   = 1.00f;
+static const float attenuation_linear     = 0.00f;
+static const float attenuation_quadtraic  = 0.02f;
 
-struct VSOut
+struct VSOut 
 {
   float3 worldPosition : POSITION;
   float3 norm : NORMAL;
@@ -34,13 +35,17 @@ float4 main(VSOut input) : SV_Target
   // Distance to light
   float distanceToLight = length(lightPosition - input.worldPosition);
   
+  // Light attenuation
   float attenuation = 1.0f / (attenuation_constant + (attenuation_linear * distanceToLight) + (attenuation_quadtraic * (distanceToLight * distanceToLight)));
   
-  float diffuse = saturate(dot(N, L));
+  float diffuse = attenuation * diffuseIntensity * max(0, dot(N, L));
   
-  float3 finalValue = diffuse * diffuseColor * attenuation * diffuseIntensity;
+  // Final pixel value
+  float3 finalValue = diffuse * diffuseColor;
   
-  return float4(finalValue, 1.0f);
+  float3 litMaterialColor = tex.Sample(smplr, input.tex).xyz * finalValue;
   
+  return float4(litMaterialColor, 1.0f);
+  // return tex.Sample(smplr, input.tex) * float4(finalValue, 1.0f);
   // return tex.Sample(smplr, input.tex);
 }
